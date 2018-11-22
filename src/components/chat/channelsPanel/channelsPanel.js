5d5dbbb7367b6d36efc6a48956e8ec9925e2e78e
Zach Niemann <czniemann1990@gmail.com>
2018-11-22 12:47:19 -0600

    added a channelsPanel component. Right now this is basically a placeholder, displayed as part of the profile path. The only functionality it offers is letting the user signout, which renders a signing-out component and deleting the token & username cookies

---
+++ src/components/chat/channelsPanel/channelsPanel.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import AuthenticationContext from '../../authentication/authenticationContext';
+
+const ChannelsPanel = ({ username, signout }) => {
+    return (
+        <section className='side-panel'>
+            <button className='link-to' onClick={signout}><small>signout</small></button>
+        </section>
+    );
+}
+
+const ChannelsPanelContext = (props) => {
+    return (
+        <AuthenticationContext.Consumer>
+            { ({ isUserAuthenticated }) => (
+                <ChannelsPanel {...props} username={isUserAuthenticated.username} />
+            )}
+        </AuthenticationContext.Consumer>
+    );
+};
+
+export default ChannelsPanelContext;
