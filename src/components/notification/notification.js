bb7ee4b130ecaaa64e33c0226f1daa546cbfdc54
Zach Niemann <czniemann1990@gmail.com>
2018-11-23 14:52:10 -0600

    added basic styling to notification

--- src/components/notification/notification.js
+++ src/components/notification/notification.js
@@ -5,7 +5,9 @@ import NotificationContext from './notificationContext/notificationContext';
 
 // returns the notification msg if it's active
 const DisplayNotification = ({ isActive, msg }) => {
-    const display = isActive ? (<div className='notification'>{msg}</div>) : null;
+    console.log('DisplayNotification() enter');
+    
+    const display = isActive ? (<p className='notification'>{msg}</p>) : null;
     return (display);
 }
 
@@ -14,8 +16,8 @@ class Notification extends Component {
         super(props);
 
         this.state = {
-            isActive: false, // determines if the notification is displayed
-            msg: '', // notification message to display
+            isActive: true, // determines if the notification is displayed
+            msg: 'This is a test TOASTER :D', // notification message to display
         };
 
         // creates the new notification
@@ -26,14 +28,19 @@ class Notification extends Component {
     }
 
     newNotification(msg, duration) {
+        console.log('newNotification(): ');
+        console.log(`\tmsg: ${msg}`);
+        console.log(`\tduration: ${duration}\n`);
+
         this.setState({
             isActive: true,
             msg: msg,
-            //duration: duration
         });
 
         // notification becomes inactive after the duration has elapsed
         setTimeout(() => {
+            console.log('newNotification deactivating');
+
             this.setState({
                 isActive: false,
                 msg: ''
