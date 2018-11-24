da9eedf7b64b883a86cb961f26808fd9e7bc5c74
Zach Niemann <czniemann1990@gmail.com>
2018-11-24 15:38:33 -0600

    added home navigation component that will be absolutely position in the top-left on /login and /create-account to navigate back to / (still needs styling and to add the back-arrow img)

---
+++ src/components/navigation/homeNavigation/homeNavigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomeNavigation = (props) => {
+    return (
+        <Link to='/' className='navigate home'>Back arrow here</Link>
+    );
+};
+
+export default HomeNavigation;
