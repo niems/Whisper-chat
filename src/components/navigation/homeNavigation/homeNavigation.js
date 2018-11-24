aed05945dd811c87f341beb0db894cb0a20df84d
Zach Niemann <czniemann1990@gmail.com>
2018-11-24 16:20:46 -0600

    the home navigation now includes a back-arrow img that translates in home when hovered over

--- src/components/navigation/homeNavigation/homeNavigation.js
+++ src/components/navigation/homeNavigation/homeNavigation.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 
 const HomeNavigation = (props) => {
     return (
-        <Link to='/' className='navigate home abs-top-left'>Back arrow here</Link>
+        <Link to='/' className='navigate home abs-top-left'>
+            <img src='./assets/svg/placeholder/round-back-arrow.svg' alt='navigate back to homepage' />
+        </Link>
     );
 };
 
