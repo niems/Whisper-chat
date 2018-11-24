b7099e21adcd7d0301a5d685c9c4bf6dc5634150
Zach Niemann <czniemann1990@gmail.com>
2018-11-24 10:58:24 -0600

    updated the semantics: changed chatview to be the <main> content

--- src/components/chat/displayUserProfile/chatView/chatView.js
+++ src/components/chat/displayUserProfile/chatView/chatView.js
@@ -4,14 +4,14 @@ import ChannelMessages from './channelMessages/channelMessages';
 
 const ChatView = ({ channelInfo, sendNewMsg }) => {
     return (
-        <section id='chat-view'>
+        <main id='chat-view'>
             <div id='channel-title'>
                 <h1>{channelInfo.name}</h1>
             </div>
 
             <ChannelMessages messages={channelInfo.messages} />
             <ChatInput channel={channelInfo.name} sendNewMsg={sendNewMsg} />
-        </section>
+        </main>
     );
 };
 
