f6e1d03ef86134e132d814374f458a060d9ccc3f
Zach Niemann <czniemann1990@gmail.com>
2018-11-29 17:03:45 -0600

    changed the channelsPanel component from a stateless function to a PureComponent: this keeps it from re-rendering when the chatView component re-renders because a new message is appended to the selected channel

--- src/components/chat/userProfile/displayUserProfile/categoriesPanel/categoriesPanel.js
+++ src/components/chat/userProfile/displayUserProfile/categoriesPanel/categoriesPanel.js
@@ -1,36 +1,35 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import CategoryList from './CategoryList/categoryList';
 
-const CategoriesPanel = ({ username, allChannelRefs, allChannels, allOnlineUsers, onChannelSelect, signout }) => {
+class CategoriesPanel extends PureComponent {
+    render() {
+        return (
+            <section className='side-panel'>
+                <button id='logout' className='link-to' onClick={this.props.signout}>
+                    <img src='./assets/svg/placeholder/logout-black.svg' alt='user logout' />
+                </button>
     
-    // get all the categories to be displayed in the panel
-   const channelCategories = Object.keys( allChannels ).map(category => (
-        <CategoryList key={category} category={category} displayCategoryTitle={true} channelRef={allChannelRefs[category]} onChannelSelect={onChannelSelect} channelList={allChannels[category]} />
-    ));
-
-    const onlineUsers =  (<CategoryList category={'online-users'} channelRef={allChannelRefs['Online Users']} onChannelSelect={onChannelSelect} channelList={allOnlineUsers} />);
-
-    return (
-        <section className='side-panel'>
-            <button id='logout' className='link-to' onClick={signout}>
-                <img src='./assets/svg/placeholder/logout-black.svg' alt='user logout' />
-            </button>
-
-            <nav id='all-categories' className='scrollbar'>
-                <h2 className='category-section-header'>Channels</h2>
-                <div className='category-section'>
-                    {channelCategories}
-                </div>
-
-                <h2 className='category-section-header'>Online Users</h2>
-                <div className='category-section'>
-                    {onlineUsers}
-                </div>
-            </nav>
-
-            <span id='current-user'>{username}</span>
-        </section>
-    );
+                <nav id='all-categories' className='scrollbar'>
+                    <h2 className='category-section-header'>Channels</h2>
+                    <div className='category-section'>
+                        { Object.keys( this.props.allChannels ).map(category => (
+                            <CategoryList key={category} category={category} displayCategoryTitle={true} channelRef={this.props.allChannelRefs[category]}
+                                          onChannelSelect={this.props.onChannelSelect} channelList={this.props.allChannels[category]} />
+                        ))}
+                    </div>
+    
+                    <h2 className='category-section-header'>Online Users</h2>
+                    <div className='category-section'>
+                        <CategoryList category={'online-users'} channelRef={this.props.allChannelRefs['Online Users']}
+                                      onChannelSelect={this.props.onChannelSelect} channelList={this.props.allOnlineUsers} />
+                    </div>
+                </nav>
+    
+                <span id='current-user'>{this.props.username}</span>
+            </section>
+        );
+    }
 }
+
 
 export default CategoriesPanel;
