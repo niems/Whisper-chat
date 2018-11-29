e3a09c0e0b440afa93f76882e212534d434b8195
Zach Niemann <czniemann1990@gmail.com>
2018-11-29 16:18:44 -0600

    changed the message item component from a stateless function to a pure component, which dramatically cuts down on re-renders.

--- src/components/chat/userProfile/displayUserProfile/chatView/channelMessages/messageItem/messageItem.js
+++ src/components/chat/userProfile/displayUserProfile/chatView/channelMessages/messageItem/messageItem.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
-const MessageItem = ({ msg }) => {
-    return (
-        <li className='channel-msg'>
-            <img className='avatar' src='./assets/svg/user-icon.svg' alt='user avatar' />
-            <div className='msg-container'>
-                <div>
-                    <span className='msg username'>{msg.username}</span>
-                    <span className='msg timestamp'>{msg.timestamp}</span>  
+class MessageItem extends PureComponent {
+    render() {
+        return (
+            <li className='channel-msg'>
+                <img className='avatar' src='./assets/svg/user-icon.svg' alt='user avatar' />
+                <div className='msg-container'>
+                    <div>
+                        <span className='msg username'>{this.props.msg.username}</span>
+                        <span className='msg timestamp'>{this.props.msg.timestamp}</span>  
+                    </div>
+                    <p className='msg text'>{this.props.msg.text}</p>
                 </div>
-                <p className='msg text'>{msg.text}</p>
-            </div>
-
-        </li>
-    );
-};
+    
+            </li>
+        );
+    }
+}
 
 export default MessageItem;
