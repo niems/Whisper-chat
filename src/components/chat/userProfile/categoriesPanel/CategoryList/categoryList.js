81f87bf1697f31c25a154ba02b8409fe87a1cd24
Zach Niemann <czniemann1990@gmail.com>
2018-11-30 16:35:47 -0600

    updated the categoryList from a stateless component to a pure component: now if any of the categoriesPanel list data changes (new PM being added), only the item from that specific category is re-rendered

--- src/components/chat/userProfile/categoriesPanel/CategoryList/categoryList.js
+++ src/components/chat/userProfile/categoriesPanel/CategoryList/categoryList.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import ChannelItem from './channelItem/channelItem';
 
 // only displays category title & toggle button if category was given
 const DisplayCategoryTitle = ({ category }) => {
-    //<input className='category-toggle' id={`${category}-toggle`} type='checkbox' />
     const categoryDisplay = category ?
         <React.Fragment>
             <h3 id={`${category}-category`} className='category-title'>{category}</h3>
@@ -13,22 +12,20 @@ const DisplayCategoryTitle = ({ category }) => {
     return categoryDisplay;
 };
 
-const CategoryList = ({ category, channelList, displayCategoryTitle = false, channelsRef = null, onChannelSelect = null }) => {
-    const channels = channelList.map(channel => (
-        <ChannelItem key={channel} channel={channel} />
-    ));
-
-    // if category list is length 0, return null (ex. PMs when a user initially creates an account)
-
-    return(
-        <React.Fragment>
-            {displayCategoryTitle ? <DisplayCategoryTitle category={category} /> : null}
-            
-            <ul id={`${category}-list`} className='category-channels-list' onClick={onChannelSelect}>
-                {channels}
-            </ul>
-        </React.Fragment>
-    )
-};
+class CategoryList extends PureComponent {
+    render() {
+        return(
+            <React.Fragment>
+                {this.props.displayCategoryTitle ? <DisplayCategoryTitle category={this.props.category} /> : null}
+                
+                <ul id={`${this.props.category}-list`} className='category-channels-list' onClick={this.props.onChannelSelect}>
+                    {this.props.channelList.map(channel => (
+                        <ChannelItem key={channel} channel={channel} />
+                    ))}
+                </ul>
+            </React.Fragment>
+        );
+    }
+}
 
 export default CategoryList;
