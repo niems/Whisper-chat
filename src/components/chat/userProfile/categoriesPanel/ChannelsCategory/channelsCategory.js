58202353639db9f14ec9f69ca5f0c55a78080544
Zach Niemann <czniemann1990@gmail.com>
2018-12-03 15:28:05 -0600

    Split up the Groups and PMs sub-categories in the categoriesPanel

---
+++ src/components/chat/userProfile/categoriesPanel/ChannelsCategory/channelsCategory.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import CategoryList from './CategoryList/categoryList';
+
+const ChannelsCategory = ({ allChannels, allChannelRefs, onChannelSelect }) => {
+    const Groups = 'Groups';
+    const PMs = 'PMs';
+
+    return (
+        <React.Fragment>
+            <h2 className='category-section-header'>Channels</h2>
+            <div className='category-section'>
+                <CategoryList category={Groups} displayCategoryTitle={true} channelRef={allChannelRefs[Groups]}
+                              onChannelSelect={onChannelSelect} channelList={allChannels[Groups]} />
+
+                <CategoryList category={PMs} displayCategoryTitle={true} channelRef={allChannelRefs[PMs]}
+                    onChannelSelect={onChannelSelect} channelList={allChannels[PMs]} />
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default ChannelsCategory;
--- src/components/chat/userProfile/categoriesPanel/categoriesPanel.js
+++ src/components/chat/userProfile/categoriesPanel/categoriesPanel.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import CategoryList from './CategoryList/categoryList';
+import ChannelsCategory from './ChannelsCategory/channelsCategory';
 import OnlineUsersCategory from './OnlineUsersCategory/onlineUsersCategory';
 
 class CategoriesPanel extends PureComponent {
@@ -13,13 +13,8 @@ class CategoriesPanel extends PureComponent {
                 </button>
     
                 <nav id='all-categories' className='scrollbar'>
-                    <h2 className='category-section-header'>Channels</h2>
-                    <div className='category-section'>
-                        { Object.keys( this.props.allChannels ).map(category => (
-                            <CategoryList key={category} category={category} displayCategoryTitle={true} channelRef={this.props.allChannelRefs[category]}
-                                          onChannelSelect={this.props.onChannelSelect} channelList={this.props.allChannels[category]} />
-                        ))}
-                    </div>
+                    <ChannelsCategory allChannels={this.props.allChannels} allChannelRefs={this.props.allChannelRefs}
+                                      onChannelSelect={this.props.onChannelSelect} />
     
                     <OnlineUsersCategory channelRef={this.props.allChannelRefs['Online Users']} onUserSelect={this.props.onChannelSelect}
                                          userList={this.props.allOnlineUsers} />
@@ -31,14 +26,4 @@ class CategoriesPanel extends PureComponent {
     }
 }
 
-
 export default CategoriesPanel;
-
-/*
-<div className='category-section'>
-                        { Object.keys( this.props.allOnlineUsers ).map(user => (
-                            <CategoryList category={'online-users'} channelRef={this.props.allChannelRefs['Online Users']}
-                                      onChannelSelect={this.props.onChannelSelect} channelList={this.props.allOnlineUsers} />
-                        ))}
-                    </div>
-*/
