cc5621c236cd4c35ef19532bba9cb8069209a055
Zach Niemann <czniemann1990@gmail.com>
2018-12-06 14:19:36 -0600

    the login-here link from the create account component now uses the urlContext

--- src/components/createAccount/createAccount.js
+++ src/components/createAccount/createAccount.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import UrlContext from '../urlContext/urlContext';
 import HomeNavigation from '../navigation/homeNavigation/homeNavigation';
 import CreateAccountForm from './createAccountForm/createAccountForm';
 
 const CreateAccount = (props) => {
     return (
-        <article id='create-account' className='wrapper center'>
-            <HomeNavigation />
-            <h1>Create Account</h1>
-            <CreateAccountForm />
-
-            <p className='related-info'>Already have an account? <Link className='link-to-default' to='/login'>Login here</Link></p>
-        </article>
+        <UrlContext.Consumer>
+            { ({ basePath }) => (
+                <article id='create-account' className='wrapper center'>
+                    <HomeNavigation />
+                    <h1>Create Account</h1>
+                    <CreateAccountForm />
+        
+                    <p className='related-info'>Already have an account? <Link className='link-to-default' to={`${basePath}login`}>Login here</Link></p>
+                </article>
+            )}
+        </UrlContext.Consumer>
     );
 };
 
