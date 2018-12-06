5cd0bd56c3611125660bb96c94e1ed1076313013
Zach Niemann <czniemann1990@gmail.com>
2018-12-06 14:26:48 -0600

    updated the login component so it now uses the urlContext

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import UrlContext from '../urlContext/urlContext';
 import HomeNavigation from '../navigation/homeNavigation/homeNavigation';
 import LoginForm from './loginForm/loginForm';
 
 const Login = (props) => {
     return (
-        <article id='login' className='wrapper center'>
-            <HomeNavigation />
-            <h1>Login</h1>
-            <LoginForm />
-
-            <p className='related-info'>
-            Think you have an account? We'll just see...<br />
-            Or create a new account <Link to='/create-account' className='link-to-default'>here</Link>
-            </p>
-        </article>
+        <UrlContext.Consumer>
+            { ({ basePath }) => (
+                <article id='login' className='wrapper center'>
+                    <HomeNavigation />
+                    <h1>Login</h1>
+                    <LoginForm />
+    
+                    <p className='related-info'>
+                    Think you have an account? We'll just see...<br />
+                    Or create a new account <Link to={`${basePath}create-account`} className='link-to-default'>here</Link>
+                    </p>
+                </article>
+            )}
+        </UrlContext.Consumer>
     );
 };
 
