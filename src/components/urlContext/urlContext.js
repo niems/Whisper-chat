76d3aba6debc3e01fcfdd55e580f78df40a4b6b5
Zach Niemann <czniemann1990@gmail.com>
2018-12-15 17:36:46 -0600

    updated the urlContext to production build for hosting on gh-pages

--- src/components/urlContext/urlContext.js
+++ src/components/urlContext/urlContext.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+
+const isProductionBuild = true; // path MUST be updated if it's the production build because of how it's hosted
 const UrlContext = React.createContext({
-  production: false,
-  basePath: ''
+  production: isProductionBuild,
+  basePath: isProductionBuild ? '/whisper-chat' : ''
 });
 
 export default UrlContext;
