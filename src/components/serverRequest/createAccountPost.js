0ed60acacb6f1ce22662c14833f2be5d0acf5b9d
Zach Niemann <czniemann1990@gmail.com>
2018-12-18 12:04:10 -0600

    post url fix: previously posting the base heroku url without adding /login or /create-account

--- src/components/serverRequest/createAccountPost.js
+++ src/components/serverRequest/createAccountPost.js
@@ -2,6 +2,6 @@ import generalAccountPost from './generalAccountPost';
 
 export default function createAccountPost(data) {
   //const authenticationURL = 'http://localhost:8080/create-account';
-  const authenticationURL = 'https://afternoon-springs-45644.herokuapp.com/';
+  const authenticationURL = 'https://afternoon-springs-45644.herokuapp.com/create-account';
   return generalAccountPost(data, authenticationURL);
 }
--- src/components/serverRequest/loginPost.js
+++ src/components/serverRequest/loginPost.js
@@ -2,6 +2,6 @@ import generalAccountPost from './generalAccountPost';
 
 export default function loginPost(data) {
   //const loginURL = 'http://localhost:8080/login';
-  const loginURL = 'https://afternoon-springs-45644.herokuapp.com/';
+  const loginURL = 'https://afternoon-springs-45644.herokuapp.com/login';
   return generalAccountPost(data, loginURL);
 }
