3d1d1dc196554f8a373c6189c1f3003f2aa9bbf8
Zach Niemann <czniemann1990@gmail.com>
2018-12-18 14:59:20 -0600

    updating the verify-account GET request for the login to heroku live version

--- src/components/serverRequest/verifyAccountGet.js
+++ src/components/serverRequest/verifyAccountGet.js
@@ -2,7 +2,9 @@
     determines if the user's token is valid before allowing profile access */
 export default function verifyAccountGet() {
   return new Promise((resolve, reject) => {
-    const verificationURL = 'http://localhost:8080/verify-token';
+    //const verificationURL = 'http://localhost:8080/verify-token';
+    const verificationURL = 'https://afternoon-springs-45644.herokuapp.com/verify-token';
+
 
     const verifyOptions = {
       method: 'GET',
