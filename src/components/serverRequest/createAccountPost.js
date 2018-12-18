cad8a4e8c9734d8720b00cc44cce408143a2f3a9
Zach Niemann <czniemann1990@gmail.com>
2018-12-18 05:05:14 -0600

    updated post url to heroku

--- src/components/serverRequest/createAccountPost.js
+++ src/components/serverRequest/createAccountPost.js
@@ -1,6 +1,7 @@
 import generalAccountPost from './generalAccountPost';
 
 export default function createAccountPost(data) {
-  const authenticationURL = 'http://localhost:8080/create-account';
+  //const authenticationURL = 'http://localhost:8080/create-account';
+  const authenticationURL = 'https://git.heroku.com/afternoon-springs-45644.git';
   return generalAccountPost(data, authenticationURL);
 }
--- src/components/serverRequest/loginPost.js
+++ src/components/serverRequest/loginPost.js
@@ -1,6 +1,7 @@
 import generalAccountPost from './generalAccountPost';
 
 export default function loginPost(data) {
-  const loginURL = 'http://localhost:8080/login';
+  //const loginURL = 'http://localhost:8080/login';
+  const loginURL = 'https://git.heroku.com/afternoon-springs-45644.git';
   return generalAccountPost(data, loginURL);
 }
