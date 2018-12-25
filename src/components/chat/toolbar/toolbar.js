fe660f9e637ed1386188c1ceea8841cd0de94989
Zach Niemann <czniemann1990@gmail.com>
2018-12-25 11:18:34 -0600

    init toolbar: the logout button, username, and channels category toggle (on small width devices) will be moved to this bar

---
+++ src/components/chat/toolbar/toolbar.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react';
+
+class Toolbar extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='toolbar'>
+      </div>
+    );
+  }
+}
+
+export default Toolbar;
